761fcb77723af70002ebdf377ad8286d55cc7a0f
adislksn <adi.120140038@student.itera.ac.id>
2023-07-05 12:55:53 +0700

    Update login, middleware, regist, route login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('id_roles')->nullable()->default(99);
+            $table->unsignedBigInteger('id_roles')->nullable()->default(88);
             $table->foreign('id_roles')->references('id')->on('roles')
             ->constrained('users')->onDelete('cascade')->onUpdate('cascade');
             $table->integer('pinpoint_id');
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login');
+Route::post('/', 'App\Http\Controllers\Auth\LoginController@login');
+
+
+Auth::routes(['login' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
